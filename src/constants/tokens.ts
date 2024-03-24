11202b5cfa4af249acf06a58861e7d51b52c2167
Ptolemy <ptolemythedev@gmail.com>
2024-03-24 15:47:38 -0400

    Changed type of PLANE token map

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -1,4 +1,4 @@
-import { ChainTokenMap, TokenMap } from '../types'
+import { TokenMap } from '../types'
 import { USDB_ADDRESS, WETH_ADDRESS, PLANETOKEN_ADDRESS } from './addresses'
 import { ChainId } from '../enums'
 import { Token } from '../entities/Token'
@@ -19,6 +19,6 @@ export const WNATIVE: TokenMap = {
   ...WETH
 }
 
-export const PLANE: ChainTokenMap = {
-  [ChainId.BLAST_SEPOLIA]: new Token(ChainId.BLAST_SEPOLIA, PLANETOKEN_ADDRESS[ChainId.BLAST_SEPOLIA], 18, 'PLANE', 'Planar token')
+export const PLANE: TokenMap = {
+  [ChainId.BLAST_SEPOLIA]: new Token(ChainId.BLAST_SEPOLIA, PLANETOKEN_ADDRESS[ChainId.BLAST_SEPOLIA], 18, 'PLANE', 'Planar Token')
 }
